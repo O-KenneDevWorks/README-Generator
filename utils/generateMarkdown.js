a54789893696b58d5f39d09ac6d21c51506089a7
O-KenneDevWorks <175995429+O-KenneDevWorks@users.noreply.github.com>
2024-09-29 19:53:50 -0400

    Resolved issue with ToC links not working

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -62,12 +62,12 @@ ${data.description}
 
 ## Table of Contents 
 
-- [Installation](#Installation)
-- [Usage](#Usage)
-- [License](#License)
-- [Contributing](#Contributing)
-- [Tests](#Tests)
-- [Questions](#Questions)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
 
 ## Installation
